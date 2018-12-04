d9e2b1f9a7bb8073d4865a8699643e146449b28f
Shahrukh <jalilshahrukh@gmail.com>
2018-12-04 16:01:46 -0500

    Updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'; 
+import ReactDOM  from 'react-dom';
 
 class App extends Component { 
+  constructor(props) { 
+    super(props); 
+    this.tick = this.tick.bind(this); 
+  }
+
+  tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('app'));
+  }
+
   render() { 
     return ( 
       <div>
-        <h1>My React App</h1>
+        {setInterval(this.tick, 1000)}
       </div>
     )
   }
